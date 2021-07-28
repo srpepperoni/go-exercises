64a27cb7e8566b7b5ab42d659d6a84f59b5b7f2f
Jaime Yera <jaimeyera@gmail.com>
2021-07-28 21:40:47 +0200

    Add comment to help understand harder part of solution

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -14,6 +14,11 @@ func main() {
 	quizGame()
 }
 
+/*
+* Main problem in part 1 of exercise 1 is compare strings, because when one of strings cames from standard input code has to verify \r and \n and due to every OS makes differents behaviours
+* for end of lines you have to trim that characters in different ways.
+ */
+
 // Exercise #1: Quiz Game
 func quizGame() {
 	recordFile, err := os.Open("./resources/problems.csv")
