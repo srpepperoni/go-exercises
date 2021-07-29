956d97ab0dc14d41535d48df0066b04960312696
Jaime Yera <jaimeyera@gmail.com>
2021-07-29 18:36:25 +0200

    Added timer to quizGame with goroutines and channels

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -8,10 +8,13 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	quizGame()
+	ch := make(chan bool)
+	go funcTimer(ch)
+	quizGame(ch)
 }
 
 /*
@@ -20,10 +23,9 @@ func main() {
  */
 
 // Exercise #1: Quiz Game
-func quizGame() {
+func quizGame(fin chan bool) {
 	recordFile, err := os.Open("./resources/problems.csv")
 	count := 0
-	total := 0
 
 	if err != nil {
 		fmt.Println("An error encountered ::", err)
@@ -33,22 +35,37 @@ func quizGame() {
 
 	inputReader := bufio.NewReader(os.Stdin)
 
+registros:
 	for _, record := range records {
 		fmt.Println(record[0])
-		answ, _ := inputReader.ReadString('\n')
 
-		if runtime.GOOS == "windows" {
-			answ = strings.TrimRight(answ, "\r\n")
-		} else {
-			answ = strings.TrimRight(answ, "\n")
+		select {
+		case end, ok := <-fin:
+			if ok {
+				if end {
+					fmt.Println("FIN")
+					break registros
+				}
+			}
+		default:
+			answ, _ := inputReader.ReadString('\n')
+
+			if runtime.GOOS == "windows" {
+				answ = strings.TrimRight(answ, "\r\n")
+			} else {
+				answ = strings.TrimRight(answ, "\n")
+			}
+
+			if strings.Compare(answ, record[1]) == 0 {
+				count++
+			}
 		}
-
-		if strings.Compare(answ, record[1]) == 0 {
-			count++
-		}
-
-		total++
 	}
 
-	fmt.Println("Correct Answers: " + strconv.Itoa(count) + "/" + strconv.Itoa(total))
+	fmt.Println("Correct Answers: " + strconv.Itoa(count) + "/" + strconv.Itoa(len(records)))
+}
+
+func funcTimer(fin chan bool) {
+	time.Sleep(3 * time.Second)
+	fin <- true
 }
