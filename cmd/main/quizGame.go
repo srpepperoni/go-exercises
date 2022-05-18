736da9263fc1fde77ede947168e11bb5ae7f0905
Jaime Yera <jaimeyera@gmail.com>
2022-05-18 21:37:26 +0200

    First part of second goExercise

---
+++ cmd/main/quizGame.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func QuizGame() {
+	ch := make(chan bool)
+	go funcTimer(ch)
+	quizGame(ch)
+}
+
+/*
+* Main problem in part 1 of exercise 1 is compare strings, because when one of strings cames from standard input code has to verify \r and \n and due to every OS makes differents behaviours
+* for end of lines you have to trim that characters in different ways.
+ */
+
+// Exercise #1: Quiz Game
+func quizGame(fin chan bool) {
+	recordFile, err := os.Open("./resources/problems.csv")
+	count := 0
+
+	if err != nil {
+		fmt.Println("An error encountered ::", err)
+	}
+	reader := csv.NewReader(recordFile)
+	records, _ := reader.ReadAll()
+
+	inputReader := bufio.NewReader(os.Stdin)
+
+registros:
+	for _, record := range records {
+		fmt.Println(record[0])
+
+		select {
+		case end, ok := <-fin:
+			if ok {
+				if end {
+					fmt.Println("FIN")
+					break registros
+				}
+			}
+		default:
+			answ, _ := inputReader.ReadString('\n')
+
+			if runtime.GOOS == "windows" {
+				answ = strings.TrimRight(answ, "\r\n")
+			} else {
+				answ = strings.TrimRight(answ, "\n")
+			}
+
+			if strings.Compare(answ, record[1]) == 0 {
+				count++
+			}
+		}
+	}
+
+	fmt.Println("Correct Answers: " + strconv.Itoa(count) + "/" + strconv.Itoa(len(records)))
+}
+
+func funcTimer(fin chan bool) {
+	time.Sleep(3 * time.Second)
+	fin <- true
+}
---
+++ cmd/main/urlShortener.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		url := pathsToUrls[r.URL.Path]
+		if url != "" {
+			http.Redirect(w, r, url, http.StatusPermanentRedirect)
+		} else {
+			fallback.ServeHTTP(w, r)
+		}
+	})
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//     - path: /some-path
+//       url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// TODO: Implement this...
+	return nil, nil
+}
+
+func UrlShortener() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+
+	mapHandler := MapHandler(pathsToUrls, mux)
+
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", mapHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
